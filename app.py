c24e04a0f42596e1f01c636728035d707c0efc27
Maxime Charpentier <pro.maxime.charpentier@gmail.com>
2021-11-24 11:10:48 +0100

    changing name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from utils import *
 
-st.title("Application de routage des camions - LPD")
+st.title("Smart Routage 🚛 🌍 - LPD")
 
 uploaded_file = st.file_uploader("Choisir un fichier de commandes")
 delay = st.slider("Choisir un délai de livraison", min_value=1, max_value=2)
