6cb2f65efc34d7072f1697f935e92a4569600dd7
Maxime Charpentier <pro.maxime.charpentier@gmail.com>
2021-11-24 16:41:27 +0100

    cleaning

--- app.py
+++ app.py
@@ -10,33 +10,29 @@ st.sidebar.header("Smart-Routage")
 uploaded_file = st.sidebar.file_uploader("Choisir un fichier de commandes")
 warehouse = st.sidebar.selectbox("Choisir un entrepôt", [None, "Cergy", "Rennes", "Épinal", "Montauban", "Avignon"])
 
-done = False
-button = False
 
-if uploaded_file and warehouse and not done:
-    button = st.button("Calculer les routes")
-if button:
-    if uploaded_file is None:
-        st.text_area("Fichier invalide !")
-    else:
-        orders = pd.read_csv(uploaded_file)
-        
-        with st.spinner('Calcul des routes optimales 🚛  ... '):
-            new_routes = optimize(orders, warehouse)
-        
-        st.success("Calcul réussi !")
-        done=True
+if uploaded_file and warehouse:
+    if st.button("Calculer les routes"):
+        if uploaded_file is None:
+            st.text_area("Fichier invalide !")
+        else:
+            orders = pd.read_csv(uploaded_file)
+            
+            with st.spinner('Calcul des routes optimales 🚛  ... '):
+                new_routes = optimize(orders, warehouse)
+            
+            st.success("Calcul réussi !")
 
-        st.download_button(
-            label="Exporter les routes dans un fichier .csv",
-            data=new_routes.to_csv().encode("utf-8"),
-            file_name="routes.csv",
-            mime="text/csv",
-        )
+            st.download_button(
+                label="Exporter les routes dans un fichier .csv",
+                data=new_routes.to_csv().encode("utf-8"),
+                file_name="routes.csv",
+                mime="text/csv",
+            )
 
-        st.write(f"{len(new_routes)} trajets à effectuer aujourd'hui !")
+            st.write(f"{len(new_routes)} trajets à effectuer aujourd'hui !")
 
-        for k, (i, route) in enumerate(new_routes.iterrows()):
-            with st.expander(f"Trajet {k+1} : {route.total_distance:.0f} km", ):
-                st.write(f"**Arrêts** : {route.stops}")
-                st.write(f"**Commandes** : {route.orders}")
+            for k, (i, route) in enumerate(new_routes.iterrows()):
+                with st.expander(f"Trajet {k+1} : {route.total_distance:.0f} km", ):
+                    st.write(f"**Arrêts** : {route.stops}")
+                    st.write(f"**Commandes** : {route.orders}")
