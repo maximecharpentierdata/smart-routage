af1796cb71d2553501e55b0315fa85ae8903de49
Maxime Charpentier <pro.maxime.charpentier@gmail.com>
2021-11-24 16:30:04 +0100

    update app

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ st.title("Calcul des routes - LPD")
 st.sidebar.header("Smart-Routage")
 
 uploaded_file = st.sidebar.file_uploader("Choisir un fichier de commandes")
-warehouse = st.selectbox("Choisir un entrepôt", ["Cergy", "Rennes", "Épinal", "Montauban", "Avignon"])
+warehouse = st.sidebar.selectbox("Choisir un entrepôt", ["Cergy", "Rennes", "Épinal", "Montauban", "Avignon"])
 
-if uploaded_file:
+if uploaded_file and warehouse:
     if st.button("Calculer les routes"):
         if uploaded_file is None:
             st.text_area("Fichier invalide !")
@@ -18,15 +18,15 @@ if uploaded_file:
             orders = pd.read_csv(uploaded_file)
             new_routes = optimize(orders, warehouse)
 
-            st.write(f"{len(new_routes)} trajets à effectuer aujourd'hui !")
+            st.write(f"**{len(new_routes)} trajets** à effectuer aujourd'hui !")
 
             for k, (i, route) in enumerate(new_routes.iterrows()):
-                with st.expander(f"Trajet {k+1} : {route.total_distance:.0f} km"):
-                    st.write(f"Arrêts : {route.stops}")
-                    st.write(f"Commandes : {route.orders}")
+                with st.expander(f"Trajet {k+1} : **{route.total_distance:.0f} km**"):
+                    st.write(f"**Arrêts** : {route.stops}")
+                    st.write(f"**Commandes** : {route.orders}")
             
             st.download_button(
-                label="Télécharger les routes",
+                label="Exporter les routes dans un fichier",
                 data=new_routes.to_csv().encode("utf-8"),
                 file_name="routes.csv",
                 mime="text/csv",
